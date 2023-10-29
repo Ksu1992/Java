52b7cc0887d635f902721d14123a40f5ff005359
Ksu1992 <145616302+Ksu1992@users.noreply.github.com>
2023-10-29 19:21:09 +0100

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Task1 {
+    public static void main(String[] args) {
+        String name = "Kseniia";
+        int age = 31;
+        double weight = 60.5;
+
+        System.out.println("Name=" + name + ", age=" + age + ", weight=" + weight);
+    }
+}
+
