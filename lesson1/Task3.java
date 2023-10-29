2f45157037faeefd59b68debf925ea1934c7676f
Kseniia Naryshkina <narushkina.ksu@gmail.com>
2023-10-29 20:28:38 +0100

    [*] Refactor code. Separate directory

---
+++ lesson1/Task3.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        System.out.println("До обміну: a = " + a + ", b = " + b);
+
+        a = a + b;  // Тепер a = 8
+        b = a - b;  // Тепер b = 5
+        a = a - b;  // Тепер a = 3
+
+        System.out.println("Після обміну: a = " + a + ", b = " + b);
+    }
+}
