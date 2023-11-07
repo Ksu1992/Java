979fb42ba9d45d91062fc9b5c53128912f3eca9d
Kseniia Naryshkina <narushkina.ksu@gmail.com>
2023-11-07 22:29:54 +0100

    WordCout

--- ua/naryshkina/practice3/WordCout.java
+++ ua/naryshkina/practice3/WordCout.java
@@ -1,7 +1,7 @@
 package ua.naryshkina.practice3;
 public class WordCout {
     public static void main(String[] args) {
-        String text = "London is the capital of England and the United Kingdom. The history of this modern city goes back to the times of the Romans. In the very center of London is the Houses of Parliament - the Palace of Westminster, as well as the famous clock tower Big Ben and Westminster Abbey. This church is the site of the coronation of British monarchs. On the opposite bank of the Thames there is the London Eye Ferris wheel, which offers panoramic views not only of the south bank, but of the entire city.";
+        String text = " text                text text  text text                        text  ";
         System.out.println("Исходный текст: " + text);
 
         String stringWithoutSpaces = removeSpaces(text);
@@ -18,7 +18,8 @@ public class WordCout {
 
     // Метод для подсчета количества слов в тексте
     public static int countWords(String text) {
-        String[] words = text.split(" ");
+        text = text.trim();
+        String[] words = text.split("\\s+");
         return words.length;
     }
 }
